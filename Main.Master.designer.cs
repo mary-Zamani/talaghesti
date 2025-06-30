b316ae21daaced4232b0c5d988a1817fd421279e
m.zamani <zamani365@gmail.com>
2025-06-30 09:03:38 +0330

    ok

--- Main.Master.designer.cs
+++ Main.Master.designer.cs
@@ -66,7 +66,7 @@ namespace narsShop
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor shopcart;
+        
 
         /// <summary>
         /// lbl_shopcart control.
